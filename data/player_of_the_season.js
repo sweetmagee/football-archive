046a96f7a2f926676b434f155672dc98b9879320
sweetmagee <jefftrice@hotmail.com>
2026-04-23 06:40:48 +0100

    Add files via upload

---
+++ data/player_of_the_season.js
@@ -0,0 +1,17 @@
+[
+  {
+    "season_id": "1896-97",
+    "team_id": "t1",
+    "player_id": ""
+  },
+  {
+    "season_id": "1897-98",
+    "team_id": "t1",
+    "player_id": ""
+  },
+  {
+    "season_id": "1898-99",
+    "team_id": "t1",
+    "player_id": ""
+  }
+]
